a7c5aef0515ed87a41f8bcbb294ad43bffa3fd57
Egor Larionov <egor.larionov@gmail.com>
2015-04-27 01:23:46 +0000

    ported to new layout

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,27 +1,28 @@
 var renderInvitableFriends = function() {
-  var list = $('#fs .scrollable_list');
+  var list = $('.content #invitee-list');
   list.children().remove('.item');
   var template = list.find('.template');
   for( var i = 0; i < friendCache.invitable_friends.length; i++ ) {
     var item = template.clone().removeClass('template').addClass('item');
     item.attr('data-id',friendCache.invitable_friends[i].id);
-    item.find('.name').html(friendCache.invitable_friends[i].name);
+    item.find('.profile').attr('title',friendCache.invitable_friends[i].name);
     item.find('.profile').attr('src',friendCache.invitable_friends[i].picture.data.url);
     list.append(item);
   }
   list.children().remove('.loading');
   list.children().remove('.template');
+  $('[data-toggle="tooltip"]').tooltip({placement : "auto", html : true})
 }
 
 var renderWelcome = function() {
-  var welcome = $('#welcome');
+  var welcome = $('#home');
   welcome.find('.first_name').html(friendCache.me.first_name);
   welcome.find('.profile').attr('src',friendCache.me.picture.data.url);
 }
 
 var onLeaderboard = function() {
   renderInvitableFriends();
-  $('#fs').removeClass('hidden');
+  //$('#fs').removeClass('hidden');
 }
 
 var showHome = function() {
